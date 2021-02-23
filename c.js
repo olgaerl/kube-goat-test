fa20363550afd28e8a417472a087aa5e1b622a24
olgaerl <erl.olga@gmail.com>
2021-02-23 23:09:45 +0200

    Create c.js

---
+++ c.js
@@ -0,0 +1,2 @@
+var endpoint = 'https://maps.googleapis.com/maps/api/geocode/json?&address='+encodeURIComponent(address_string)+'&key=AIzaSyBv1TkdxvWkbFaDz2r0Yx7xvlNKe-2uyRc';
+console.log(endpoint);
